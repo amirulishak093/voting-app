23b5c9a490f0a98e6e5484244d2602a8b08be069
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-06 10:46:07 +0800

    Update +page.server.ts

--- src/routes/manageVoting/+page.server.ts
+++ src/routes/manageVoting/+page.server.ts
@@ -1,6 +1,6 @@
 import type { Actions } from "@sveltejs/kit";
 import type { RequestEvent } from "../$types";
-import { getContestants } from "$lib/server/services/contestant";
+import { getContestants, toggleEnabled } from "$lib/server/services/contestant";
 
 export async function load(event: RequestEvent) {
   const contestants = await getContestants();
@@ -9,5 +9,25 @@ export async function load(event: RequestEvent) {
 }
 
 export const actions: Actions = {
-  default: async ({ request }) => {},
+  default: async ({ request }) => {
+    const formData = await request.formData();
+
+    const contestantId = formData.get('contestantId') as string;
+
+    try {
+      await toggleEnabled(contestantId)
+
+      return {
+        success: true,
+        status: 'Form is submitted'
+      }
+
+    } catch (error) {
+      console.log(error)
+
+      return {
+        error
+      }
+    }
+  },
 };
