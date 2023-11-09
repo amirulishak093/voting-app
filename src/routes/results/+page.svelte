f4ef17ecd30d78b81aaec8f334a2ed54f4c804ba
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-09 09:30:25 +0800

    Update +page.svelte

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -98,7 +98,7 @@
 		<canvas bind:this={chartContext} class="w-full mt-4 bg-white rounded-lg shadow-lg" />
 
 		<div class="flex justify-center mt-8">
-		 	<img class="w-60" src={'/MGT-VOTING.png'} />
+		 	<img class="w-80" src={'/MGT-VOTING.png'} />
 		</div>
 
 	</div>
